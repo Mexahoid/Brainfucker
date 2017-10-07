012ed3cb108cd2abee991a9a4bbf312fdf7ba0f4
Mexahoid <mirramax97@Mail.ru>
2017-10-07 21:46:07 +0300

    Выффы

--- Brainfucker/Program.cs
+++ Brainfucker/Program.cs
@@ -10,15 +10,12 @@ namespace Brainfucker
         private static void Main(string[] args)
         {
             ASTree ast = new ASTree();
-            Interpreter bf = new Interpreter();
             string src = Console.ReadLine();
             try
             {
                 ast.CreateTree(src);
                 ast.PrintTree();
                 ast.Evaluate();
-                
-                //bf.Interpret(src);
             }
             catch (Exception e)
             {
